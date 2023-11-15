fb81f25eb9f0b0bc71fa04c926400ec611030811
Mandeep Patel <mandeeppatel1988@gmail.com>
2023-11-14 19:38:38 -0800

    Update: Updated the Color Scheme to One Dark Pro

--- .config/nvim/lua/arealdeepman/plugins/colorscheme.lua
+++ .config/nvim/lua/arealdeepman/plugins/colorscheme.lua
@@ -1,7 +1,11 @@
 return {
 	-- preferred color scheme
 	{
-		"joshdick/onedark.vim",
+    -- "One Dark Theme for Neo Vim"
+		-- "joshdick/onedark.vim",
+
+    -- Closest Copy of One Dark Pro for VSCode by Binaryify
+    "olimorris/onedarkpro.nvim",
 		lazy = false, -- make sure we load this during startup
 		priority = 1000, -- setting high priority for theme
 		config = function()
